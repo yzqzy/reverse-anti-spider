9d5201d3a38ecc04a5b2113c5dc8d2a751c60530
heora <yueluo.yang@gmail.com>
2024-04-13 22:50:47 +0800

    perf: update

--- src/server/service/cookie-plus-verify.ts
+++ src/server/service/cookie-plus-verify.ts
@@ -3,35 +3,35 @@
 import { getRandomString, jsonStringify, parseCookie } from '@shared/tools.js'
 import { desDecrypt, base64Decrypt } from '@shared/encrypt.js'
 
+const desKey = 'wyc.F=!po95TQ]2?c!~C1sW>*DCC>*YA3+237%YH'
+
 const unauthorized = (res: any) => {
   const jsCode = `<script>
 import('https://cdn.jsdelivr.net/npm/crypto-es@2.1.0/+esm')
   .then(module => {
     const CryptoJS = module.default;
-    setTimeout(() => {
-      const desEncrypt = (text, key) => {
-        const keyHex = CryptoJS.enc.Hex.parse(key);
-        const encrypted = CryptoJS.DES.encrypt(text, keyHex, {
-          mode: CryptoJS.mode.ECB,
-          padding: CryptoJS.pad.Pkcs7
-        });
-        return encrypted.toString();
-      };
-      const parseCookie = (cookie) => {
-        if (!cookie) return {}
-        const cookies = cookie.split(';')
-        const result = {}
-        for (const c of cookies) {
-          const [key, value] = c.trim().split('=')
-          result[key] = value
-        }
-        return result
+    const desEncrypt = (text, key) => {
+      const keyHex = CryptoJS.enc.Hex.parse(key);
+      const encrypted = CryptoJS.DES.encrypt(text, keyHex, {
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7
+      });
+      return encrypted.toString();
+    };
+    const parseCookie = (cookie) => {
+      if (!cookie) return {}
+      const cookies = cookie.split(';')
+      const result = {}
+      for (const c of cookies) {
+        const [key, value] = c.trim().split('=')
+        result[key] = value
       }
-      const time = parseCookie(document.cookie).sid;
-      const text = btoa('${getRandomString()}') + '&' + time;
-      document.cookie = 'token=' + btoa(desEncrypt(text, 'wyc.F=!po95TQ]2?c!~C1sW>*DCC>*YA3+237%YH'));
-      location.reload();
-    }, 60)
+      return result
+    }
+    const time = parseCookie(document.cookie).sid;
+    const text = btoa('${getRandomString()}') + '&' + time;
+    document.cookie = 'token=' + btoa(desEncrypt(text, '${desKey}'));
+    location.reload();
   })
   .catch(error => {
     console.error(error);
@@ -54,10 +54,7 @@ export default function (req: any, res: any, next: any) {
       return unauthorized(res)
     }
 
-    const decryptedText = desDecrypt(
-      token,
-      'wyc.F=!po95TQ]2?c!~C1sW>*DCC>*YA3+237%YH'
-    )
+    const decryptedText = desDecrypt(token, desKey)
     console.log(`[cookie plus verify] Decrypted text: ${decryptedText}`)
 
     const serverTime = decryptedText.split('&')[1]
@@ -68,7 +65,7 @@ export default function (req: any, res: any, next: any) {
     const isExpired = Math.abs(currentTime - +serverTime) > 5 * 1000
     console.log(`[cookie plus verify] Is expired: ${isExpired}`)
 
-    if (isExpired) {
+    if (!serverTime || isExpired) {
       return unauthorized(res)
     }
 
